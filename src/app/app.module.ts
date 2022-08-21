a0c862a4419b2aa841666bd485491a285083e3f6
claudio <claudiobenitez107@hotmail.com>
2022-08-21 13:02:22 -0300

    mi primer commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,38 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { EncabezadoComponent } from './componente/encabezado/encabezado.component';
+import { LogoAPComponent } from './componente/logo-ap/logo-ap.component';
+import { SocialComponent } from './componente/social/social.component';
+import { BannerComponent } from './componente/banner/banner.component';
+import { AcercaDeComponent } from './componente/acerca-de/acerca-de.component';
+import { ExperienciaComponent } from './componente/experiencia/experiencia.component';
+import { EducacionComponent } from './componente/educacion/educacion.component';
+import { NgCircleProgressModule } from 'ng-circle-progress';
+import { HysComponent } from './componente/hys/hys.component';
+import { ProyectoComponent } from './componente/proyecto/proyecto.component';
+import { FooterComponent } from './componente/footer/footer.component';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EncabezadoComponent,
+    LogoAPComponent,
+    SocialComponent,
+    BannerComponent,
+    AcercaDeComponent,
+    ExperienciaComponent,
+    EducacionComponent,
+    HysComponent,
+    ProyectoComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgCircleProgressModule.forRoot({}),
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
